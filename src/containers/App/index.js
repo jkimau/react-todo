04f9c1c95dd9c53c813d008de1b7b1e9a44d7c9c
jkimau <jkim.au@gmail.com>
2017-04-10 17:16:33 +1000

    App.js moved os App/index.js

---
+++ src/containers/App/index.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import FlexContainer from 'components/FlexContainer';
+import Search from 'components/Search';
+import TodoRow from 'components/TodoRow';
+
+const TopMenuBar = styled.div`
+  height: 46px;
+  border-bottom: 1px solid rgba(0,0,0,0.15);
+`;
+
+const Dummy = styled.div`
+  width: 30%;
+`;
+
+const MainTitle = styled.div`
+  flex-grow: 1;
+`;
+
+const MainContentBody = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: #f5f8fa;
+`;
+
+const MainContentWrapper = styled.div`
+  margin: 0 auto;
+  width: 590px;
+`;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.todoMock = [{
+      id: 1,
+      title: 'test1',
+      date: '2017-4-20T00:00:00Z',
+      details: 'test details'
+    }];
+  }
+
+  render() {
+    return (
+      <div>
+        <TopMenuBar>
+          <FlexContainer direction="row">
+            <Dummy />
+            <MainTitle>Sick TODO</MainTitle>
+            <Search width="30%" test="test" />
+          </FlexContainer>
+        </TopMenuBar>
+        <MainContentBody>
+          <MainContentWrapper>
+            {this.todoMock.map((todo) => <TodoRow key={todo.id} todo={todo} />)}
+          </MainContentWrapper>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </MainContentBody>
+      </div>
+    );
+  }
+}
+
+export default App;
