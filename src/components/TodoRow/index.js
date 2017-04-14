1a5da092fffbaa2b545e40071eff202c61e9d3c6
jkimau <jkim.au@gmail.com>
2017-04-14 21:05:46 +1000

    updated TodoRow as class component / open close state added

--- src/components/TodoRow/index.js
+++ src/components/TodoRow/index.js
@@ -1,40 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 
-import { mainBG, lightGreyBorder } from 'global/colors';
+import { mainBG, todoRowBorder } from 'global/colors';
 
 const padding = `15px`;
 
 const TodoRowWrapper = styled.div`
   background: white;
-  border: 1px solid ${lightGreyBorder};
+  border-bottom: 1px solid ${todoRowBorder};
+
+  &:last-child {
+    border-bottom: none;
+  }
 `;
 
 const Header = styled.div`
-  border-bottom: 1px solid #e6ecf0;
   padding: ${padding}
 `;
 
 const Body = styled.div`
-  padding: ${padding};
   cursor: pointer;
+  height: 0;
+  padding: 0;
+  opacity: 0;
 
   &:hover {
     background: ${mainBG};
   }
 `;
 
-const TodoRow = ({ todo: { title, date, details } }) => {
-  return (
-    <TodoRowWrapper>
-      <Header>
-        <span>{title}</span><span>{date}</span>
-      </Header>
-      <Body>
-        {details}
-      </Body>
-    </TodoRowWrapper>
-  );
-};
+const OpenClose = styled.span`
+  float: right;
+  cursor: pointer;
+`;
 
-export default TodoRow;
+export default class TodoRow extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { detailOpen: false };
+
+    this.toggleDetailsOpenStatus = this.toggleDetailsOpenStatus.bind(this);
+  }
+
+  toggleDetailsOpenStatus() {
+    this.setState(prevState => ({ detailOpen: !prevState.detailOpen }));
+  }
+
+  render() {
+    const { todo: { title, date, details } } = this.props;
+
+    return (
+      <TodoRowWrapper>
+        <Header>
+          <span>{title}</span>
+          <span>{date}</span>
+          <OpenClose
+            onClick={this.toggleDetailsOpenStatus}>
+            {this.state.detailOpen ? 'close' : 'open'}
+          </OpenClose>
+        </Header>
+        <Body>
+          {details}
+        </Body>
+      </TodoRowWrapper>
+    );
+  }
+};
