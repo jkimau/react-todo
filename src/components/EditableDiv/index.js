097538d1b5bde69277e780a4e81afaf969029ea2
jkimau <jkim.au@gmail.com>
2017-04-15 22:24:42 +1000

    updated EditableDiv to have palceholder and take first line wrapped in div correctly

--- src/components/EditableDiv/index.js
+++ src/components/EditableDiv/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 
 const EditableDivWrapper = styled.div`
@@ -10,9 +10,10 @@ const EditableDivWrapper = styled.div`
   box-sizing: border-box;
 `;
 
-const EditableDiv = styled.div`
+const EditableDivision = styled.div`
   float: right;
   width: 508px;
+  min-height: ${props => props.editingMode ? '60px' : 'auto'};
   padding: 8px 10px;
   background: white;
   border: 1px solid #CBEFFB;
@@ -26,21 +27,86 @@ const EditableDiv = styled.div`
   }
 `;
 
-const TextArea = styled.textarea`
+const HiddenTextArea = styled.textarea`
   display: none;
 `;
 
-export default () => {
-  return (
-    <EditableDivWrapper>
-      <form>
-        <EditableDiv
-          contentEditable={true}
-          suppressContentEditableWarning={true}>
-          Add your plan
-        </EditableDiv>
-        <TextArea />
-      </form>
-    </EditableDivWrapper>
-  );
+const FormButtonsWrapper = styled.div`
+  clear: both;
+  display: block;
+  padding-top: 5px;
+  display: ${props => props.editingMode ? 'block' : 'none'};
+`;
+
+const Button = styled.button`
+  float: right;
+  color: white;
+  font-size: 14px;
+  padding: 8px 18px;
+  background: rgba(47,194,239,.8);
+  border-radius: 5px;
+  border-style: none;
+  cursor: pointer;
+  opacity:
+
+  &:hover {
+    background: #2FC2EF;
+  }
+`;
+
+const DummyDiv = styled.div`
+
+`;
+
+export default class EditableDiv extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { editingMode: false };
+    this.enterEditingMode = this.enterEditingMode.bind(this);
+    this.enterWaitingMode = this.enterWaitingMode.bind(this);
+    this.validateForm = this.validateForm.bind(this);
+  }
+
+  enterEditingMode() {
+    if (!this.state.editingMode) {
+      this.setState(prevState => ({ editingMode: true }));
+    }
+  }
+
+  enterWaitingMode() {
+    if (this.state.editingMode) {
+      this.setState(prevState => ({ editingMode: false }));
+    }
+  }
+
+  validateForm() {
+    console.log('ad');
+  }
+
+  render() {
+    return (
+      <EditableDivWrapper>
+        <form>
+          <div>
+            <EditableDivision
+              contentEditable={true}
+              suppressContentEditableWarning={true}
+              onFocus={this.enterEditingMode}
+              onBlur={this.enterWaitingMode}
+              editingMode={this.state.editingMode}
+              onKeyPress={this.validateForm}
+              data-placeholder="adsfafd">
+              {/* <DummyDiv><br/></DummyDiv> */}
+              <div><br/></div>
+            </EditableDivision>
+            <HiddenTextArea />
+          </div>
+          <FormButtonsWrapper editingMode={this.state.editingMode}>
+            <Button editingMode={this.state.editingMode}>Add</Button>
+          </FormButtonsWrapper>
+        </form>
+      </EditableDivWrapper>
+    );
+  }
 };
--- src/global/style.js
+++ src/global/style.js
@@ -7,4 +7,13 @@ injectGlobal`
     padding: 0;
     font-family: sans-serif;
   }
+
+  [contenteditable=true]:not(:focus):before {
+    content: attr(data-placeholder);
+    display: block; /* For Firefox */
+  }
+
+  [contenteditable=true]:not(:focus) > div:first-child {
+    display: none;
+  }
 `;
