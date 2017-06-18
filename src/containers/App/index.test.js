b2297d1be2f75842c0f97f1a06602ddaab3aeacb
jkimau <jkim.au@gmail.com>
2017-06-18 22:25:41 +1000

    (#4) test for App added

--- src/containers/App/index.test.js
+++ src/containers/App/index.test.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount} from 'enzyme';
 import renderer from 'react-test-renderer';
 import { MemoryRouter } from 'react-router-dom';
 
-// import TopNavigation from 'components/TopNavigation';
-// import SearchInput from 'components/Search';
-// import ListView from 'containers/ListView';
-// import CalendarView from 'containers/CalendarView';
 import { App } from '../App';
+
+jest.mock('containers/ListView');
+jest.mock('containers/CalendarView');
 
 describe('<App/>', () => {
   const defaultProps = {
@@ -15,7 +14,7 @@ describe('<App/>', () => {
     closeAllTodoMenus: jest.fn(),
     setViewMode: jest.fn(),
     location: {
-      pathname: 'fake_pathname'
+      pathname: 'list'
     }
   };
 
@@ -23,14 +22,32 @@ describe('<App/>', () => {
     return shallow(<App {...props}/>)
   };
 
-  // test('snapshot with view mode list', () => {
-  //   const tree = renderer.create(
-  //     <MemoryRouter>
-  //       <App {...defaultProps}/>
-  //     </MemoryRouter>
-  //   ).toJSON();
-  //   expect(tree).toMatchSnapshot();
-  // });
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  test('snapshot - list view mode', () => {
+    const tree = renderer.create(
+      <MemoryRouter>
+        <App {...defaultProps}/>
+      </MemoryRouter>
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('snapshot - list view mode', () => {
+    const newProps = Object.assign(
+      {},
+      defaultProps,
+      { viewMode: 'calendar' }
+    );
+    const tree = renderer.create(
+      <MemoryRouter>
+        <App {...newProps}/>
+      </MemoryRouter>
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
   test('should call setInitialViewMode() on load', () => {
     App.prototype.setInitialViewMode = jest.fn();
@@ -38,6 +55,62 @@ describe('<App/>', () => {
 
     expect(App.prototype.setInitialViewMode).toHaveBeenCalled();
   });
+
+  // test('should call setInitialViewMode() on load', () => {
+  //   const spy = jest.spyOn(App.prototype.setInitialViewMode);
+  //   compileComponent();
+  //
+  //   expect(App.prototype.setInitialViewMode).toHaveBeenCalled();
+  //   spy.mockRestore();
+  // });
+
+  test('should call changeViewModeHandler()', () => {
+    App.prototype.changeViewModeHandler = jest.fn();
+    const wrapper = mount(
+      <MemoryRouter>
+        <App {...defaultProps}/>
+      </MemoryRouter>
+    );
+
+    wrapper.find('#calendar-nav-link').simulate('click');
+
+    expect(App.prototype.changeViewModeHandler).toHaveBeenCalled();
+  });
+
+  test('should call documentClickHandler()', () => {
+    const event = {
+      target: {
+        className: 'todo-menu-trigger'
+      }
+    };
+
+    const wrapper = compileComponent();
+    wrapper.instance().documentClickHandler(event);
+
+    expect(wrapper.instance().props.closeAllTodoMenus).not.toHaveBeenCalled();
+
+    const newEvent = {
+      target: {
+        className: 'random-class-name'
+      }
+    };
+
+    wrapper.instance().documentClickHandler(newEvent);
+
+    expect(wrapper.instance().props.closeAllTodoMenus).toHaveBeenCalled();
+  });
+
+  // test('should run setInitialViewMode() properly', () => {
+  //   const newProps = Object.assign(
+  //     {},
+  //     defaultProps,
+  //     { location: { pathname: '/list' } }
+  //   );
+  //   const wrapper = compileComponent(newProps);
+  //   wrapper.instance().setInitialViewMode();
+  //
+  //   expect(wrapper.instance().props.setViewMode).toHaveBeenCalledWith('list');
+  // });
 
   test('should render <Route/> properly', () => {
     const wrapper = compileComponent();
