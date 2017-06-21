a4ce043dd5547d19b6c033eab515b8f39a4304e7
jkimau <jkim.au@gmail.com>
2017-06-21 21:18:41 +1000

    (#15) redux devTool added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk';
 
 import combinedReducer from 'reducers';
@@ -11,7 +11,10 @@ import 'global/style';
 
 let store = createStore(
   combinedReducer,
-  applyMiddleware(thunk)
+  compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
 );
 
 ReactDOM.render(
